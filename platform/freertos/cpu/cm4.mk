3283f9767d899f4a38a45f5d41ac7e9fba2af3af
Werner Almesberger <werner@almesberger.net>
2013-06-25 04:24:54 -0300

    Makefile.freertos, cpu/cm4.mk: remove CDEPFLAGS, STM32OCM3, clean up comments

--- platform/freertos/cpu/cm4.mk
+++ platform/freertos/cpu/cm4.mk
@@ -7,8 +7,9 @@
 ARCH_FLAGS = -mcpu=cortex-m4 -mthumb -march=armv7e-m \
              -mfloat-abi=hard -mfpu=fpv4-sp-d16
 
+STM32OCM3 = clock.c
 TARGETLIBS = random.c
 
-CONTIKI_TARGET_SOURCEFILES += $(STM32OCM3) $(TARGETLIBS)
+CONTIKI_TARGET_SOURCEFILES += $(TARGETLIBS)
 
 PREFIX = arm-none-eabi
